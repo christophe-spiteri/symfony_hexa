515edc7baf8b86e3e9c6d63fceaf115528f46762
christophe <christophe@cls-scripts.com>
2022-07-24 06:25:16 +0200

    scss

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
 
@@ -56,8 +56,9 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
-
+    .enableSassLoader()
+    .enableLessLoader()
+    .enableStylusLoader()
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
