abde0dfa0f24054912f1f939b8df2c1fd96c7fb0
christophe <christophe@cls-scripts.com>
2022-07-24 07:42:25 +0200

    popperjs, tooltip, dropdown boostrap

--- assets/app.js
+++ assets/app.js
@@ -7,10 +7,17 @@
 
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
-
 // start the Stimulus application
 import './bootstrap';
-
 // or you can include specific pieces
-require('bootstrap/js/dist/tooltip');
-require('bootstrap/js/dist/popover');
+// You can specify which plugins you need
+global.bootstrap=require('bootstrap');
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
